222e2057e9aeb4cf3644372fae708dd8831fd802
Ra99e <Rasmus.brege@gmail.com>
2025-04-22 18:29:42 +0200

    Spin-name Fix
    Spin being correctly called spin everywhere instead of swing in the pathSegments script

--- Assets/OurScripts/pathSegments.cs
+++ Assets/OurScripts/pathSegments.cs
@@ -37,7 +37,7 @@ public class PathSegment
     public float gravity = 9.81f;
     public float maxPushForce = 1f;
 
-    [Header("Swing-inställningar")]
+    [Header("Spin-inställningar")]
     public bool enableSpin = true;
     public float rotationSpeed = 0f;
 }
