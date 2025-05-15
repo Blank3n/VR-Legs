7e2b5e34c4178079a2828b6fe84fc8b70deb119d
Blank <anton-blank@outlook.com>
2025-05-15 13:24:37 +0200

    Slalom done

--- Assets/SetStartPos.cs
+++ Assets/SetStartPos.cs
@@ -5,7 +5,7 @@ public class SetStartPos : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.position = new Vector3(-40.9f, 0.24f, -264f);
+        transform.position = new Vector3(0f, 0f, 0f);
         transform.rotation = Quaternion.Euler(0f, 270f, 0f);
     }
 
