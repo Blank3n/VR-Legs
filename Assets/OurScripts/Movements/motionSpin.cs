70489ac799335f4b94293ecae09c642dcf930914
Ra99e <Rasmus.brege@gmail.com>
2025-05-27 11:52:23 +0200

    Movements done!

--- Assets/OurScripts/Movements/motionSpin.cs
+++ Assets/OurScripts/Movements/motionSpin.cs
@@ -5,17 +5,27 @@ public class motionSpin : MonoBehaviour
     public bool enableSpin = false;
     public float rotSpeed = 1f; // Rotations per 5 seconds
 
+    private bool enabled = false;
+    private float compAmount = 0;
     void Update()
     {
         if (!enableSpin)
         {
             transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            enabled = false;
             return;
         }
 
         float rotationAmount = (rotSpeed / 5f) * 360f * Time.time;
 
-        transform.localRotation = Quaternion.Euler(0f, rotationAmount, 0f);
+        if (!enabled)
+        {
+            enabled = true;
+            compAmount = -rotationAmount;
+        }
+
+        float finalRotation = rotationAmount + compAmount;
+        transform.localRotation = Quaternion.Euler(0f, finalRotation, 0f);
     }
 }
 
--- Assets/SetStartPos.cs
+++ Assets/SetStartPos.cs
@@ -5,8 +5,8 @@ public class SetStartPos : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.position = new Vector3(0, 0.5f, 0);
-        transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        transform.position = new Vector3(60, 0f, 0);
+        transform.rotation = Quaternion.Euler(0f, 270f, 0f);
     }
 
     // Update is called once per frame
